85d4cb639ee09d2d4aa23399e3f937540cff6fa9
Sami Virtanen <djyv-sep@protonmail.com>
2025-01-24 23:17:22 +0200

    pictures added

--- gamejam2025/bubble.gd
+++ gamejam2025/bubble.gd
@@ -2,6 +2,14 @@ extends Node2D
 class_name Bubble
 var tileType=1
 var board:Board
+
+
+func update_gfx(type):
+	if(type == 1):
+		$BubbleGfx.set_texture(load("res://resources/pillar_player1.png"))
+	elif (type == 3):
+		$BubbleGfx.set_texture(load("res://resources/pillar_player2.png"))
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	board=get_tree().get_root().get_node("root/Board")
@@ -15,6 +23,7 @@ func _process(delta: float) -> void:
 
 
 func _on_button_button_down() -> void:
+	update_gfx(tileType)
 	if(board.turnOrder[board.currentTurn]!=board.Players.AUTOMATA):
 		print(tileType)
 		tileType=board.turnOrder[board.currentTurn]
