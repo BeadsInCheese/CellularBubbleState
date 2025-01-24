5c9370fa40583f4c847b464f2ec25759a5e56963
Mikko <36571102+BeadsInCheese@users.noreply.github.com>
2025-01-24 21:16:15 +0200

    new stuff

--- gamejam2025/Grid.gd
+++ gamejam2025/Grid.gd
@@ -9,6 +9,16 @@ var xsize : int = 20
 var ysize : int = 20
 
 var gridList=[]
+enum Players{PLAYER1,PLAYER2,AUTOMATA}
+var turnOrder=[Players.PLAYER1,Players.PLAYER2,Players.AUTOMATA,Players.PLAYER2,Players.PLAYER1,Players.AUTOMATA]
+var currentTurn=0
+
+
+
+func changeTurn()->void:
+	currentTurn=(currentTurn+1)%6
+	if(turnOrder[currentTurn]==Players.AUTOMATA):
+		pass
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for i in range(xsize):
@@ -18,8 +28,13 @@ func _ready() -> void:
 			
 			add_child(x)
 			gridList.append(x)
+	moveToplace()
 	
-	
+func moveToplace()->void:
+	for i in range(len(gridList)):
+		var xpos:int=i%xsize
+		var ypos:int=i/ysize
+		gridList[i].position=Vector2(xpos,ypos)*24+Vector2(20,20)
 func automata_step() -> void:
 	for i in range(len(gridList)):
 		var xpos:int=i%xsize
@@ -31,3 +46,7 @@ func automata_step() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_button_button_down() -> void:
+	automata_step()
