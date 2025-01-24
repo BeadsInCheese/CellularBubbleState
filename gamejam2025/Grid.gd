43e6e59765fc2e4b44e2db84ec4257fbc057649f
Mikko <36571102+BeadsInCheese@users.noreply.github.com>
2025-01-24 19:38:34 +0200

    changes

--- gamejam2025/Grid.gd
+++ gamejam2025/Grid.gd
@@ -8,7 +8,9 @@ var ysize : int = 20
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
+	
+func automata_step() -> void:
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- gamejam2025/PlayerController.gd
+++ gamejam2025/PlayerController.gd
@@ -5,7 +5,10 @@ extends Node2D
 func _ready() -> void:
 	pass # Replace with function body.
 
-
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed():
+		print("test "+str(event.global_position))
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
