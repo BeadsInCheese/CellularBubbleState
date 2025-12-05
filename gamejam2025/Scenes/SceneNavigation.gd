498ae4eaec674ca46e67d3e07bd463e674fcc6c6
BeadsInCheese <hirvittavanhauskaa@gmail.com>
2025-12-05 21:43:31 +0200

    fixed spamming breaking loading

--- gamejam2025/Scenes/SceneNavigation.gd
+++ gamejam2025/Scenes/SceneNavigation.gd
@@ -30,8 +30,12 @@ func _on_PlayerMenuPressed() -> void:
 	goToScene("res://PlayerSelect.tscn")
 
 
+var loading=false
 func goToScene(scene: String):
 	# Wait hide animation
+	if loading:
+		return
+	loading=true
 	for i in range(51):
 		RenderingServer.global_shader_parameter_set("tt",i*0.02)
 		if(get_tree()!=null):
@@ -41,7 +45,7 @@ func goToScene(scene: String):
 	if(get_tree()!=null):
 		await get_tree().process_frame
 		get_tree().change_scene_to_packed(load(scene))
-
+	loading=false
 func showall():
 	for i in range(51):
 		RenderingServer.global_shader_parameter_set("tt",1-i*0.02)
