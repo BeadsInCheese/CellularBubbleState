b0a6b9f04ed46b22fd9841b4ef07d17699c13093
BeadsInCheese <hirvittavanhauskaa@gmail.com>
2025-12-10 22:32:46 +0200

    Added console functionality

--- gamejam2025/console.gd
+++ gamejam2025/console.gd
@@ -13,7 +13,19 @@ func _exit_tree() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-func printToConsole(author:String,msg:String):
-	$log.text+=author+": "+msg+"\n"
-func systemPrintToConsole(msg:String):
-	printToConsole("System",msg)
+func write(author:String,msg:String,end:String="\n"):
+	$VBoxContainer/log.text+=author+ ("" if author=="" else ": ")+msg+end
+func systemWrite(msg:String):
+	write("System",msg)
+	
+func commandProcessor(cmd:String):
+	var args=cmd.split(" ")
+	if(args[0]=="time"):
+		systemWrite(Time.get_datetime_string_from_system())
+	if(args[0]=="help"):
+		systemWrite("commands:\n/time gets time\n/help get information about available commands")
+func _on_input_text_submitted(new_text: String) -> void:
+	write("Player",new_text)
+	$VBoxContainer/input.text=""
+	if(new_text[0]=="/"):
+		commandProcessor(new_text.substr(1))
