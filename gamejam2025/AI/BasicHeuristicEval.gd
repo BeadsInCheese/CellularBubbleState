3849fb8a1fcbce999899ade2d9836ed74b5d27fa
BeadsInCheese <hirvittavanhauskaa@gmail.com>
2025-12-18 18:44:04 +0200

    renamed variables to be more descriptive

--- gamejam2025/AI/BasicHeuristicEval.gd
+++ gamejam2025/AI/BasicHeuristicEval.gd
@@ -7,26 +7,27 @@ func calcFromPos(index:int,boardX:int)->Vector2:
 	return Vector2(index%boardX,floor(index/boardX))
 func calcToPos(index:Vector2,boardX:int)->int:
 	return index.y*boardX+index.x
-func diagonal_heuristic(observation:Array,ii)->int:
+func diagonal_heuristic(observation:Array,tileToEvaluate)->int:
 	var offsets:Array[Vector2]=[Vector2(1,1),Vector2(-1,-1),Vector2(-1,1),Vector2(1,-1)]
 	var offsets2:Array[Vector2]=[Vector2(1,0),Vector2(-1,0),Vector2(0,1),Vector2(0,-1)]
 	var score=0
 	#print(playerType)
 	for i in offsets:
-		var p = calcToPos(i+calcFromPos(ii,ob.xsize),ob.xsize)
-		if(p<observation.size()):
-			if(observation[p]==playerType):
+		var offsetTile = calcToPos(i+calcFromPos(tileToEvaluate,ob.xsize),ob.xsize)
+		if(offsetTile<observation.size()):
+			if(observation[offsetTile]==playerType):
 				score+=4
 	for i in offsets2:
-		var p = calcToPos(i+calcFromPos(ii,ob.xsize),ob.xsize)
-		if(p<observation.size()):
-			if(observation[p]==playerType+1):
+		var offsetTile = calcToPos(i+calcFromPos(tileToEvaluate,ob.xsize),ob.xsize)
+		if(offsetTile<observation.size()):
+			if(observation[offsetTile]==playerType+1):
 						score+=1
-			if(observation[p]==(playerType+2)%5+floor((playerType+2)/5)):
+			if(observation[offsetTile]==(playerType+2)%5+floor((playerType+2)/5)):
 						score-=3
-			
 	return score
-func CAN_SPAWN_MORE_THAN_TWO(observation:Array,ii)->int:
+	
+
+func CAN_SPAWN_MORE_THAN_TWO(observation:Array,tileToEvaluate)->int:
 	#.automataAgent.simulateAutomataStep(tempBoard)
 	var prevPoints:int=0
 	var newPoint:int=0
@@ -38,6 +39,7 @@ func CAN_SPAWN_MORE_THAN_TWO(observation:Array,ii)->int:
 	MoveScore=0
 	prevPoints=0
 	newPoint=0
+	#Calculate difference in points for both players
 	for i in range(observation.size()):
 			if observation[i]==playerType:
 				newPoint+=15
@@ -48,7 +50,7 @@ func CAN_SPAWN_MORE_THAN_TWO(observation:Array,ii)->int:
 			elif observation[i]==(playerType+2)%5+floor((playerType+2)/5):
 				prevPoints+=6
 	return (newPoint-prevPoints)
-func center_heuristic(observation:Array,ii)->int:
+func center_heuristic(observation:Array,tileToEvaluate)->int:
 	#aa_cache.simulateAutomataStep(observation)
 	#aa_cache.simulateAutomataStep(observation)
 	var score=0
@@ -56,28 +58,29 @@ func center_heuristic(observation:Array,ii)->int:
 	for i in range(observation.size()):
 		if(observation[i]!=0):
 			
-			var l=(((Vector2(ob.xsize/2,ob.ysize/2)-calcFromPos(i,ob.xsize)).length()))
+			var distanceToCenter=(((Vector2(ob.xsize/2,ob.ysize/2)-calcFromPos(i,ob.xsize)).length()))
 			
 			if observation[i]==playerType:
-				score+=l
+				score+=distanceToCenter
 			elif observation[i]==playerType+1:
-				score+=l
+				score+=distanceToCenter
 			elif observation[i]==(playerType+2)%5+floor((playerType+2)/5):
-				score-=l
+				score-=distanceToCenter
 			elif observation[i]==(playerType+3)%5+floor((playerType+2)/5):
-				score-=l
+				score-=distanceToCenter
 			count+=1
 			#print(l," calc ",calcFromPos(i,ob.xsize))
 	return -score
 	
 var heuristics:Array[Callable]=[center_heuristic,diagonal_heuristic,CAN_SPAWN_MORE_THAN_TWO]
-func test_heuristics(ii:int)->int:
+func test_heuristics(tileToEvaluate:int)->int:
 	var val=0
 	for i in heuristics:
 		var dup=ob.getBoardCopy()
-		dup[ii]=playerType
-		val+=i.call(dup,ii)
+		dup[tileToEvaluate]=playerType
+		val+=i.call(dup,tileToEvaluate)
 	return val
+
 func makeMove(observation:Board):
 	var offset=0
 	await observation.get_tree().create_timer(0.1).timeout
@@ -89,8 +92,6 @@ func makeMove(observation:Board):
 	ob=observation
 	for i in len(observation.gridList):
 		if(observation.gridList[i].tileType==0):
-			
-			
 			var val=test_heuristics(i)
 			if(best_score<=val):
 				best_move=i
@@ -98,6 +99,7 @@ func makeMove(observation:Board):
 	observation.gridList[(best_move)].setTileType(playerType)
 	moveMade.emit(observation)
 # Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	pass # Replace with function body.
 
@@ -105,5 +107,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
 func get_custom_class_name():
 	return "HeuristicAI"
