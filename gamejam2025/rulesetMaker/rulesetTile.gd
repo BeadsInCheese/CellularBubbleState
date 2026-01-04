9935d0cfcc51dd13da52f557c3334fe10d543b1a
BeadsInCheese <hirvittavanhauskaa@gmail.com>
2026-01-04 21:20:09 +0200

    added -1 to tiles

--- gamejam2025/rulesetMaker/rulesetTile.gd
+++ gamejam2025/rulesetMaker/rulesetTile.gd
@@ -3,18 +3,18 @@ extends TextureButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	texture_normal=load(textures[tileType])
+	texture_normal=load(textures[tileType+1])
 
-var tileType=0
+var tileType=-1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-var textures=["res://resources/GFX/Bubble icons/bubble_player1.png","res://resources/GFX/towers/pillar_player1.png","res://resources/GFX/Bubble icons/bubble03b.png","res://resources/GFX/towers/pillar_player2.png","res://resources/GFX/Bubble icons/bubble03b_2.png"]
+var textures=["res://resources/GFX/Bubble icons/bubble02b_2.png","res://resources/GFX/Bubble icons/bubble_player1.png","res://resources/GFX/towers/pillar_player1.png","res://resources/GFX/Bubble icons/bubble03b.png","res://resources/GFX/towers/pillar_player2.png","res://resources/GFX/Bubble icons/bubble03b_2.png"]
 func updateUI():
-	texture_normal=load(textures[tileType])
+	texture_normal=load(textures[tileType+1])
 	
 func _on_pressed() -> void:
-	tileType=(tileType+1)%5
+	tileType=(tileType+2)%6-1
 	updateUI()
